75a38e121a1e6d20340f56c9e654e615889cf9f6
Moksh <132830737+Moxxi-M@users.noreply.github.com>
2025-02-09 22:57:01 +0000

    Optimised + Internal Commentary
    PracticePython.org
    Exercise #3

---
+++ Lists.py
@@ -0,0 +1,9 @@
+a = [1,1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+print( [i for i in a if i < 5])
+                    # ^^--- does what the bottom two lines in one line, 
+#for i in a:           # ---> For loop for an array, in this instant each loop I will be the value of the corresponding array slot 
+    #if i < 5:print(i) # ---> Prints if its less than 5 
+
+
+
--- OddOrEven.py
+++ OddOrEven.py
@@ -3,24 +3,30 @@ num = 0 #int
 x = 0 #int 
 
 
-num = int(input("Please enter a positive number"))
+num = int(input("Please enter a positive number")) #assigning value to var and asking for input 
 
+#input validation 
 while num < 0 :
-    num = int(input("Error! Please enter a postiive number"))
+    num = int(input("Error! Please enter a postiive number")) #error message output 
 
-x = int(input("Please enter another positive number"))
+x = int(input("Please enter another positive number")) #assigning value to var and asking for input 
 
+#input validation 
 while x < 0 :
-    x = int(input("Error! Please enter a postiive number"))
+    x = int(input("Error! Please enter a postiive number")) #error message output 
 
+
+#proccesses / outputs 
+#checking if num evenly divides into x 
 if num % x == 0:
-    print(x, "divides evenly into", num)
+    print(x, "divides evenly into", num) #output 
 else:
-    print(x, "does not evenly divide into", num)
+    print(x, "does not evenly divide into", num) #output 
 
+#checking if num is a multiple of four 
 if num % 4 == 0: 
-    print(num , " is an even number, and a multiple of four")
-elif num % 2 == 0:
-    print(num, " is an even number")
-else: 
-    print(num, "is an odd number")
+    print(num , " is an even number, and a multiple of four") #output 
+elif num % 2 == 0: #checking if num is even 
+    print(num, " is an even number") #output 
+else: #if not even, it has to be odd 
+    print(num, "is an odd number") #output 
