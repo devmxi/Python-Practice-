55b3563c0483873cd8c2851183806ac6c249b664
Moksh <132830737+devmxi@users.noreply.github.com>
2025-02-23 20:44:56 +0000

    correcting my logic

--- Udemy-100-Days-Of-Code/00 Daily-Projects/12 - GuessingGame.py
+++ Udemy-100-Days-Of-Code/00 Daily-Projects/12 - GuessingGame.py
@@ -4,13 +4,13 @@ RANDNUM = random.randint(1,100)
 guessnum = 0 
 difficulty = ""
 lives = 0
-GameOver = False
+gameOver = False
 
 def chooseDifficulty():
     '''Gets the user to choose a difficulty, and assigns lives based on it'''
     difficulty = str(input("Choose a difficulty: Type 'easy' or 'hard': ")).lower()\
         
-    while difficulty != "easy" and difficulty != "hard":
+    while difficulty not in ["easy","hard"]:
         difficulty = str(input("ERROR. Please choose a difficulty: Type 'easy' or 'hard': ")).lower()
         
     if difficulty == "easy":
@@ -18,19 +18,25 @@ def chooseDifficulty():
     else: 
         return 5
     
-def guess(guessamt):
+def getguess(guessamt, lives):
     '''Gets User to guess a number, then outputs if its too high or too low, returns if they got it right or not.'''
-    global RANDNUM, lives
+    global RANDNUM
     
     guessamt = int(input("Please make a guess: "))
     
-    if lives != 1:
+    if lives == 0:
+            print("You have 0 lives remaining. You lose.") #* loss output
+            return True
+    
+    elif lives != 1:
         if guessamt > RANDNUM:
             print("Too high. \nGuess again.")
+            lives -= 1
             return False
         
         elif guessamt < RANDNUM:
             print("Too low. \nGuess again.")
+            lives -= 1
             return False
         else:
             print("Correct! You got it right!")
@@ -43,15 +49,9 @@ print("Welcome to the Number Guessing Game! \nI'm thinking of a number between 1
 #TODO check difficulty and assign lives
 lives = chooseDifficulty()
 
-while GameOver == False:
+while gameOver == False:
     print(f"You have {lives} attempts remaining to guess the number.")
     #TODO get user guess
-    GameOver = guess(guessnum) #* assgigning to result of guess 
-
-    if guess != RANDNUM: #*checking if guess is wrong
-        lives -= 1 #* losing life for wrong guess
-        if lives == 0:
-            print("You have 0 lives remaining. You lose.") #* loss output
-            GameOver = True
-        
+    gameOver = getguess(guessnum) #* assgigning to result of guess 
+              
 #12/100 - this was a challange but we got through it!! 
