8d493f183b7539bd9d99d7a0803a800cb005cebf
Moksh <132830737+devmxi@users.noreply.github.com>
2025-03-02 21:05:29 +0000

    Create OOP.py

---
+++ Udemy-100-Days-Of-Code/16 Day Sixteen/OOP.py
@@ -0,0 +1,12 @@
+#Object Orented Programming
+
+from turtle import Turtle, Screen #* importing turtle obj from turtle 
+
+
+jim = Turtle() #* jim is an object, turtle is class
+jim.shape("turtle")
+jim.color("chartreuse4")
+My_screen = Screen()
+My_screen.exitonclick() #* accessing moethod from my_screen
+print(My_screen.canvheight) #* accesssing canvheight attrabute
+
